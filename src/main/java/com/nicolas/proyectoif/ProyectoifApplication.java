a003da14b4957c0b1c7cfeaf6148f561191ebe3c
Nicolas <espitia.nicolas7@gmail.com>
2020-12-10 19:54:49 -0500

    Initial commit

---
+++ src/main/java/com/nicolas/proyectoif/ProyectoifApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.proyectoif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoifApplication.class, args);
+    }
+
+}
